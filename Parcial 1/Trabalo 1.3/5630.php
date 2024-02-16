31dea2d28416bddf0f284e2bcf28866b12132a59
DiegoGo217 <159971949+DiegoGo217@users.noreply.github.com>
2024-02-17 00:59:53 +0100

    Add files via upload

---
+++ Parcial 1/Trabalo 1.3/5630.php
@@ -0,0 +1,13 @@
+<html>
+<head></head>
+    <body>
+          <h1>5630. Colegiatura</h1>
+          <h2>Descripcion</h2>
+          <h4>
+            -La colegiatura de los alumnos se determina según el numero de materias que cursan. 
+            El costo de cada una de las materias es de $1600. <br>
+            -Se ha establecido un programa para estimular a los alumnos, 
+            el cual consiste en lo siguiente: si el promedio obtenido por un alumno en el ultimo periodo es mayor o igual que 9, se le hará un descuento del 30% sobre la colegiatura y no se le cobrara IVA; si el promedio obtenido es menor que 9 deberá pagar la colegiatura completa, mas el IVA del 16%.
+          </h4>  
+    </body>
+</html>
