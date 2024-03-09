ae1988c9a61c875c80582bf2b87edd6cc11d71b1
DiegoGo217 <159971949+DiegoGo217@users.noreply.github.com>
2024-03-09 00:02:39 -0600

    Add files via upload

---
+++ Parcial 1/Trabalo1.7/portada.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>portada</title>
+        <link rel="stylesheet" href="estilo.css">
+    </head>
+    <body>
+        <header>
+            <h1>Portada Proyecto 1.7</h1>
+            <h2>Problemas de omega up</h2>
+            <h5>realizado por: Diego Alejandro</h5>
+        </header>
+        <div class="container-navbar">
+            <nav class="navbar container">
+                <li class="fa-solid fa-bars"></li>
+                <ul class="menu">
+                    <li><a href="portada.php">portada</a></li>
+                    <li><a href="calculos-condicionales.php">calculos-condicionales</a></li>
+                    <li><a href="escalera.php">escalera</a></li>
+                    <li><a href="formulota.php">formulota</a></li>
+                    <li><a href="el-lado.php">El lado</a></li>
+                </ul>
+            </nav>
+        </div>
+        <section>
+            <h2>
+                En resumen de este trabajo y proyecto, el trabajo consciste en 4 problemas de omegaup realizados y solucionados por mi para uste.
+            </h2>
+            <img src="img/omegaup_curves.png" alt="omegaup">
+            <table class="lic" border="1">
+                <tr cellspading="2">
+                    <td></td>
+                    <td>Coder del mes:</td>
+                    <td>score:</td>
+                    <td>Resueltos:</td>
+                </tr>
+                <tr>
+                    <td><img src="img/lic german.jpg" alt="licgerman"></td>
+                    <td class="cod">Luis German Gutierrez Torres</td>
+                    <td>300000000.67</td>
+                    <td>1000000000000000000</td>
+                </tr>
+            </table>
+        </section>
+    </body>
+</html>
